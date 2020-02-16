cf20ad842d04783fcc29c31e2edd9d7e89d67835
campagno <campagno.eduardo@outlook.com>
2020-02-16 19:46:54 -0300

    ene vaquitas

---
+++ eneVaquitas.py
@@ -0,0 +1,57 @@
+import os
+import random
+import time
+import threading
+
+inicioPuente = 10
+largoPuente = 20
+
+semaVaca = threading.Semaphore(2)
+
+class Vaca(threading.Thread):
+  def __init__(self):
+    super().__init__()
+    self.posicion = 0
+    self.velocidad = random.uniform(0.1, 0.5)
+
+  def avanzar(self):
+    time.sleep(self.velocidad)
+    self.posicion += 1
+
+  def dibujar(self):
+    print(' ' * self.posicion + "🐮")
+
+  def run(self):
+    while(True):
+
+      if (inicioPuente-1 == self.posicion):
+            semaVaca.acquire()
+      
+      if (30 == self.posicion):
+            semaVaca.release()
+
+
+      self.avanzar()
+
+cantVaquitas = 8
+vacas = []
+for i in range(cantVaquitas):
+  v = Vaca()
+  vacas.append(v)
+  v.start()
+
+def cls():
+  os.system('cls' if os.name=='nt' else 'clear')
+
+def dibujarPuente():
+  print(' ' * inicioPuente + '=' * largoPuente)
+
+while(True):
+  cls()
+  print('Apretá Ctrl + C varias veces para salir...')
+  print()
+  dibujarPuente()
+  for v in vacas:
+    v.dibujar()
+  dibujarPuente()
+  time.sleep(0.2)
